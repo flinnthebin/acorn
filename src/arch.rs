a0ad2cf46384fecb5ea7793c9249a6d8cb4f1d65
Chris Flinn <christopher.flinn@gmail.com>
2024-09-11 18:45:59 +1000

    even more

--- src/arch.rs
+++ src/arch.rs
@@ -1,6 +1,61 @@
 use core::arch::asm;
 
-// Control and Status Register (CSR)
+// Control and Status Register (CSR) Addresses
+
+// Machine Level
+const MHARTID: usize = 0xf14;
+const MSTATUS: usize = 0x300;
+const MEDELEG: usize = 0x302;
+const MIDELEG: usize = 0x303;
+const MIE: usize = 0x304;
+const MCOUNTEREN: usize = 0x306;
+const MENVCFG: usize = 0x30A;
+const MEPC: usize = 0x341;
+const MCYCLE: usize = 0xB00;
+// Supervisor Level
+const SSTATUS: usize = 0x100;
+const SIE: usize = 0x104;
+const STVEC: usize = 0x105;
+const SEPC: usize = 0x141;
+const SCAUSE: usize = 0x142;
+const STVAL: usize = 0x143;
+const SIP: usize = 0x144;
+const SATP: usize = 0x180;
+// Core Local Interruptor Address (Access with CSRR/CSRW)
+const STIMECMP: usize = 0x14d;
+// Physical Memory Protection
+const PMPCFG0: usize = 0x3A0;
+const PMPADDR0: usize = 0x3B0;
+
+// Read some value from a CSR register
+macro_rules! read_csr {
+    ($csr:expr) => {{
+        let value: usize;
+        unsafe {
+            asm!(
+                "csrr {0}, {1}",
+                out(reg) value,
+                const $csr,
+                options(nostack, preserves_flags)
+            );
+        }
+        value
+    }};
+}
+
+// Write some value to a CSR register
+macro_rules! write_csr {
+    ($csr:expr, $val:expr) => {{
+        unsafe {
+            asm!(
+                "csrw {0}, {1}",
+                const $csr,
+                in(reg) $val as usize,
+                options(nostack, preserves_flags)
+            );
+        }
+    }};
+}
 
 //  __  __            _     _                  _                   _
 // |  \/  | __ _  ___| |__ (_)_ __   ___      | |    _____   _____| |
@@ -9,26 +64,15 @@ use core::arch::asm;
 // |_|  |_|\__,_|\___|_| |_|_|_| |_|\___|     |_____\___| \_/ \___|_|
 //
 
-// For RISC-V architecture, Machine Hardware Thread ID (MHARTID) CSR address = 0xf14
-const CSR_MHARTID: usize = 0xf14;
-
-// Returns ID of hardware thread
+// Returns Machine Hardware Thread ID
 pub fn read_mhartid() -> usize {
-    let hartid: usize;
-    unsafe {
-        asm!(
-            "csrr {0}, {1}",
-            out(reg) hartid,
-            const CSR_MHARTID,
-        );
-    }
-    hartid
+    read_csr!(MHARTID)
 }
 
-// Machine Status Register (MSTATUS) (CSR)
+// Machine Status Register (MSTATUS)
 // - Machine Previous Privilege (MPP[1:0]): 2-bit field indicating the previous privilege mode (M/S/U) before a trap
 
-const MSTATUS_MPP_MASK: usize = 0b11 << 11; // Mask to isolate the MPP field
+const MPP_MASK: usize = 0b11 << 11; // Mask to isolate the MPP field
 
 #[repr(usize)]
 #[derive(Copy, Clone)]
@@ -39,40 +83,100 @@ pub enum MStatVal {
     MIE = 1 << 3,     // Machine Interrupt Enable (1 = Enabled, 0 = Disabled)
 }
 
-// Returns current value of MSTATUS
 pub fn read_mstatus() -> usize {
-    let status: usize;
-    unsafe {
-        asm!(
-            "csrr {0}, mstatus",
-            out(reg) status,
-        );
-    }
-    status
+    read_csr!(MSTATUS)
 }
 
-// Writes some value to MSTATUS
 pub fn write_mstatus(val: MStatVal) {
-    unsafe {
-        asm!(
-            "csrw mstatus, {0}",
-            in(reg) val as usize,
-        );
-    }
+    write_csr!(MSTATUS, val)
+}
+
+// Machine Exception Delegation
+// Delegates exceptions from machine mode to supervisor mode
+
+pub fn read_medeleg() -> usize {
+    read_csr!(MEDELEG)
+}
+
+pub fn write_medeleg(val: usize) {
+    write_csr!(MEDELEG, val)
+}
+
+// Machine Interrupt Delegation
+// Delegates interrupts from machine mode to supervisor mode
+//
+
+pub fn read_mideleg() -> usize {
+    read_csr!(MIDELEG)
+}
+
+pub fn write_mideleg(val: usize) {
+    write_csr!(MIDELEG, val)
+}
+
+// Machine Interrupt Enable
+// Controls the enabling/disabling of various interrupts in machine mode
+
+#[repr(usize)]
+#[derive(Copy, Clone)]
+pub enum MieVal {
+    // Machine Level Machine-Mode
+    MSIE = 0b01 << 3,  // Software
+    MTIE = 0b01 << 7,  // Timer (Hardware)
+    MEIE = 0b01 << 11, // External (Hardware [I/O])
+    // Software Level Machine-Mode
+    SSIE = 0b01 << 1, // Software
+    STIE = 0b01 << 5, // Timer (Hardware)
+    SEIE = 0b01 << 9, // External (Hardware [I/O])
+}
+
+pub fn read_mie() -> usize {
+    read_csr!(MIE)
+}
+
+pub fn write_mie(val: MieVal) {
+    write_csr!(MIE, val)
+}
+
+// Machine-Mode Counter Enable
+// Controls the availability of performance counters (cycle, time, instruction) to lower privilege modes
+
+pub fn read_mcounteren() -> usize {
+    read_csr!(MCOUNTEREN)
+}
+
+pub fn write_mcounteren(val: usize) {
+    write_csr!(MCOUNTEREN, val)
+}
+
+// Machine Environment Configuration
+// Configures environment settings i.e. memory protection attributes, cacheability
+
+pub fn read_menvcfg() -> usize {
+    read_csr!(MENVCFG)
+}
+
+pub fn write_menvcfg(val: usize) {
+    write_csr!(MENVCFG, val)
 }
 
 // Machine Exception Program Counter
 // Holds the address of an instruction that caused a machine-level exception
-// Address is saved when exception occurs and can be used to resume exceution or handle the exception
+// Address is saved when exception occurs and can be used to resume execution or handle the exception
 
-// Writes instruction address to MEPC
+pub fn read_mepc() -> usize {
+    read_csr!(MEPC)
+}
+
 pub fn write_mepc(addr: usize) {
-    unsafe {
-        asm!(
-            "csrw mepc, {0}",
-            in(reg) addr,
-        );
-    }
+    write_csr!(MEPC, addr)
+}
+
+// Machine-Mode Cycle Counter
+// Read-only register. Counts number of processor clock cycles since reset
+
+pub fn read_mcycle() -> usize {
+    read_csr!(MCYCLE)
 }
 
 //  ____                              _                     _                   _
@@ -82,7 +186,7 @@ pub fn write_mepc(addr: usize) {
 // |____/ \__,_| .__/ \___|_|    \_/ |_|___/\___/|_|       |_____\___| \_/ \___|_|
 //             |_|
 
-// Supervisor Status Register (SSTATUS) (CSR)
+// Supervisor Status Register (SSTATUS)
 #[repr(usize)]
 #[derive(Copy, Clone)]
 pub enum SStatVal {
@@ -93,89 +197,153 @@ pub enum SStatVal {
     UIE = 0b01 << 0,  // User Interrupt Enable
 }
 
-// Read current value of SSTATUS
 pub fn read_sstatus() -> usize {
-    let status: usize;
-    unsafe {
-        asm!(
-            "csrr {0}, sstatus",
-            out(reg) status,
-        );
-    }
-    status
+    read_csr!(SSTATUS)
 }
 
-// Write some value to SSTATUS
 pub fn write_sstatus(val: SStatVal) {
-    unsafe {
-        asm!(
-            "csrw sstatus, {0}",
-            in(reg) val as usize,
-        );
-    }
-}
-
-// Supervisor Interrupt Pending (CSR)
-#[repr(usize)]
-#[derive(Copy, Clone)]
-pub enum SipVal {
-    SSIP = 0b01 << 1, // Software
-    STIP = 0b01 << 5, // Timer (Hardware)
-    SEIP = 0b01 << 9, // Exterrnal (Hardware [I/O])
-}
-
-// Read current state of SIP
-pub fn read_sip() -> usize {
-    let pend: usize;
-    unsafe {
-        asm!(
-            "csrr {0}, sip",
-            out(reg) pend,
-        );
-    }
-    pend
-}
-
-// Write some value to SIP
-pub fn write_sip(val: SipVal) {
-    unsafe {
-        asm!(
-            "csrw sip, {0}",
-            in(reg) val as usize,
-        );
-    }
+    write_csr!(SSTATUS, val)
 }
 
 // Supervisor Interrupt Enable
+// Controls the enabling/disabling of various interrupts in supervisor mode
+
 #[repr(usize)]
 #[derive(Copy, Clone)]
 pub enum SieVal {
     SSIE = 0b01 << 1, // Software
     STIE = 0b01 << 5, // Timer (Hardware)
-    SEIE = 0b01 << 9, // Exterrnal (Hardware [I/O])
+    SEIE = 0b01 << 9, // External (Hardware [I/O])
 }
 
-const SIE_SSIE: usize = 0b01 << 1; // Software
-const SIE_STIE: usize = 0b01 << 5; // Timer (Hardware)
-const SIE_SEIE: usize = 0b01 << 9; // External (Hardware [I/O])
-
-// Read current state of SIE
 pub fn read_sie() -> usize {
-    let interrupt: usize;
-    unsafe {
-        asm!("csrr {0}, sie",
-            out(reg) interrupt,
-        );
-    }
-    interrupt
+    read_csr!(SIE)
 }
 
-// Write some value to SIE
 pub fn write_sie(val: SieVal) {
-    unsafe {
-        asm!(
-            "csrw sie, {0}",
-            in(reg) val as usize,
-        )
-    }
+    write_csr!(SIE, val)
+}
+
+// Supervisor Trap-Vector Base Address
+// Sets base address of trap handler routine for supervisor mode
+
+pub fn read_stvec() -> usize {
+    read_csr!(STVEC)
+}
+
+pub fn write_stvec(val: usize) {
+    write_csr!(STVEC, val)
+}
+
+// Supervisor Exception Program Counter
+// Holds the address of an instruction that caused a supervisor-level exception
+// Address is saved when exception occurs prior to trap handler routine. Can be used to resume execution or handle the exception
+
+pub fn read_sepc() -> usize {
+    read_csr!(SEPC)
+}
+
+pub fn write_sepc(val: usize) {
+    write_csr!(SEPC, val)
+}
+
+// Supervisor Trap Cause
+// Holds cause of last trap (exception/interrupt) occurence in supervisor mode
+
+pub fn read_scause() -> usize {
+    read_csr!(SCAUSE)
+}
+pub fn write_scause(val: usize) {
+    write_csr!(SCAUSE, val)
+}
+
+// Supervisor Trap Value
+// Contains exception-specific information (address fault, etc) to assist debugging/exception handling
+
+pub fn read_stval() -> usize {
+    read_csr!(STVAL)
+}
+
+pub fn write_stval(val: usize) {
+    write_csr!(STVAL, val)
+}
+
+// Supervisor Interrupt Pending
+// Each register bit corresponds to a specific interrupt type
+// If set, interrupt is pending and waiting to be serviced
+
+#[repr(usize)]
+#[derive(Copy, Clone)]
+pub enum SipVal {
+    SSIP = 0b01 << 1, // Software
+    STIP = 0b01 << 5, // Timer (Hardware)
+    SEIP = 0b01 << 9, // External (Hardware [I/O])
+}
+
+pub fn read_sip() -> usize {
+    read_csr!(SIP)
+}
+
+pub fn write_sip(val: SipVal) {
+    write_csr!(SIP, val)
+}
+
+// Supervisor Address Translation and Protection
+// Manages address translation/protection, page table configuration and ASIDs
+// Integral component in supervisor mode establishment of virtual memory space
+
+// RISC-V Sv39 Page Table Schema
+const SATP_SV39: usize = 8 << 60;
+
+fn make_satp(pagetable: usize) -> usize {
+    SATP_SV39 | (pagetable >> 12)
+}
+
+pub fn read_satp() -> usize {
+    read_csr!(SATP)
+}
+
+pub fn write_satp(val: usize) {
+    write_csr!(SATP, val)
+}
+
+// Supervisor Timer Comparison
+// Memory-mapped register in Core Local Interruptor (CLINT), not defined in standard CSR set
+// Triggers timer interrupts for supervisor mode when STIME == STIMECMP
+
+pub fn read_stimecmp() -> usize {
+    read_csr!(STIMECMP)
+}
+
+pub fn write_stimecmp(val: usize) {
+    write_csr!(STIMECMP, val)
+}
+
+//  ____  _               _           _   __  __
+// |  _ \| |__  _   _ ___(_) ___ __ _| | |  \/  | ___ _ __ ___   ___  _ __ _   _
+// | |_) | '_ \| | | / __| |/ __/ _` | | | |\/| |/ _ \ '_ ` _ \ / _ \| '__| | | |
+// |  __/| | | | |_| \__ \ | (_| (_| | | | |  | |  __/ | | | | | (_) | |  | |_| |
+// |_|   |_| |_|\__, |___/_|\___\__,_|_| |_|  |_|\___|_| |_| |_|\___/|_|   \__, |
+//              |___/                                                      |___/
+
+// Physical Memory Protection Configuration Register 0
+// Configures regions 0-3 of PMP, controls permission settings (r/w/x) + addressing mode
+
+pub fn read_pmpcfg0() -> usize {
+    read_csr!(PMPCFG0)
+}
+
+pub fn write_pmpcfg0(val: usize) {
+    write_csr!(PMPCFG0, val)
+}
+
+// Physical Memory Protection Address Register 0
+// Specifies the address boundary for PMP region 0
+
+pub fn read_pmpaddr0() -> usize {
+    read_csr!(PMPADDR0)
+}
+
+pub fn write_pmpaddr0(val: usize) {
+    write_csr!(PMPADDR0, val)
 }
